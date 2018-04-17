01bb72165f3f3a679e32f805f670b17a4d17fd16
ryo kato <ryo@ryo-no-MacBook-Air.local>
2018-04-18 08:18:52 +0900

    add form

--- task/urls.py
+++ task/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'task'
 urlpatterns = [
-    url(r'^$', views.task_list, name='task_list')
+    url(r'^$', views.task_list, name='list'),
+    url(r'^task/(?P<task_id>[0-9]+)$', views.task_detail, name='detail'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Task
-
+from .forms import TaskForm
 
 def task_list(request):
 
     tasks = Task.objects.all().order_by('created_at')
     return render(request, 'task/task_list.html', {'tasks': tasks})
+
+
+def task_detail(request, task_id):
+    task = Task.objects.get(pk=task_id)
+
+    if request.method == 'POST':
+        form = TaskForm(data=request.POST, instance=task)
+        if form.is_valid():
+            return redirect('task:list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'task/task_detail.html', {'form': form})
