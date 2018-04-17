14b63067f256f6085a1a45037991f3cb92c2c8c1
ryo kato <ryo@ryo-no-MacBook-Air.local>
2018-04-18 07:28:37 +0900

    一覧表示/css

--- task/views.py
+++ task/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def task_list(request):
+
+    tasks = Task.objects.all().order_by('created_at')
+    return render(request, 'task/task_list.html', {'tasks': tasks})
