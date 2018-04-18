1c57d4e01afd7c9adc1b8abdb637437037e58cef
ryokato <ryook.github@gmail.com>
2018-04-18 19:57:22 +0900

    failed reffer to base.html

--- article/views.py
+++ article/views.py
@@ -1,29 +1,11 @@
+import requests
+
 from django.shortcuts import render
-from .models import Article
 
 
 def get_articles():
-    # import requests/
-    # res = requests.get('http://search.dev.n8s.jp', )
-    # APIGW経由
-    res = [
-        {
-            "title": "search1",
-            "body": "body1",
-            "kiji_id": "s1"
-        },
-        {
-            "title": "search2",
-            "body": "body2",
-            "kiji_id": "s2"
-        },
-        {
-            "title": "search3",
-            "body": "body3",
-            "kiji_id": "s3"
-        }
-    ]
-    return res
+    res = requests.get('https://search.dev.n8s.jp/search/v1/article?volume=5')
+    return res.json()['hits'][:5]
 
 def article_list(request):
 
