a4921ad6ace13ca936966ded40ee6f989b84a95e
ryo kato <ryo@ryo-no-MacBook-Air.local>
2018-04-20 02:10:06 +0900

    削除実装まで

--- myTodo/task/urls.py
+++ myTodo/task/urls.py
@@ -6,4 +6,6 @@ app_name = 'task'
 urlpatterns = [
     url(r'^$', views.task_list, name='list'),
     url(r'^task/(?P<task_id>[0-9]+)$', views.task_detail, name='detail'),
+    url(r'^task/new$', views.task_new, name='new'),
+    url(r'^task/(?P<task_id>[0-9]+)/delete$', views.task_delete, name='delete'),
 ]
--- myTodo/task/views.py
+++ myTodo/task/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.http import Http404
+from django.shortcuts import render, redirect
+
 from .models import Task
 from .forms import TaskForm
 
 def task_list(request):
-
     tasks = Task.objects.all().order_by('created_at')
     return render(request, 'task/task_list.html', {'tasks': tasks})
 
@@ -19,3 +20,25 @@ def task_detail(request, task_id):
     else:
         form = TaskForm(instance=task)
     return render(request, 'task/task_detail.html', {'form': form})
+
+
+def task_new(request):
+
+    if request.method == 'GET':
+        form = TaskForm()
+        return render(request, 'task/task_detail.html', {'form': form})
+    else:
+        form = TaskForm(request.POST)
+        form.is_valid()
+        form.save()
+        return  redirect('task:list')
+
+
+def task_delete(request, task_id):
+
+    if request.method == 'POST':
+        try:
+            Task.objects.get(pk=task_id).delete()
+            return redirect('task:list')
+        except Task.DoesNotExist:
+            raise Http404
