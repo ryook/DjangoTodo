0acb9e3a3120b65535b99b057bf96316d3c5b25e
ryo.kato <ryook.github@gmail.com>
2018-04-26 05:57:52 +0900

    step2

--- myTodo/frontends/assets/js/list-component.js
+++ myTodo/frontends/assets/js/list-component.js
@@ -3,24 +3,8 @@ import {TaskComponent} from './task-component';
 
 export class ListComponent extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            todos: [
-                {
-                    id: 1,
-                    name: "react"
-                },
-                {
-                    id: 2,
-                    name: "django"
-                }
-            ]
-        }
-    }
-
     render() {
-        const todos = this.state.todos.map( todo =>
+        const todos = this.props.todos.map( todo =>
                 <TaskComponent key={todo.id} {...todo}/>
             );
 
--- myTodo/frontends/assets/js/main.js
+++ myTodo/frontends/assets/js/main.js
@@ -5,13 +5,29 @@ import ReactDOM from 'react-dom';
 import {ListComponent} from './list-component';
 
 class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {
+                    id: 1,
+                    name: "react"
+                },
+                {
+                    id: 2,
+                    name: "django"
+                }
+            ]
+        }
+    }
+
   render() {
     return (
       <div>
         <h1>My Todos</h1>
         {/*<SubComponent name="My Counter for Babel" />*/}
-        <ListComponent/>
-
+        <ListComponent todos={this.state.todos}/>
       </div>
     );
   }
