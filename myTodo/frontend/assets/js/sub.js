c4924937351c2211dd0ee34488bba4b66642b514
ryokato <ryook.github@gmail.com>
2018-04-26 12:34:01 +0900

    install react

--- myTodo/frontend/assets/js/sub.js
+++
@@ -1,3 +0,0 @@
-export function hello() {
-    alert('done hello method');
-}
--- myTodo/frontend/webpack.config.js
+++ myTodo/frontend/webpack.config.js
@@ -8,6 +8,28 @@ module.exports = {
       path: path.resolve('./assets/webpack_bundles//'),
       filename: "[name]-[hash].js"
   },
+    module: {
+      rules: [
+          {
+              test: /\.js$/,
+              use: [
+                  {
+                      loader: 'babel-loader',
+                      options: {
+                          presets: [
+                              // env を指定することで、ES2017 を ES5 に変換。
+                // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
+                // webpack の Tree Shaking 機能が使えない
+                              ['env', {'modules': false}],
+                              // React の JSX を解釈
+                              'react'
+                          ]
+                      }
+                  }
+              ]
+          }
+      ]
+    },
     plugins: [
         new BundleTracker({filename: './webpack-stats-js.json'})
     ]
